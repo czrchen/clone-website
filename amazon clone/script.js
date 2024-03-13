86a5a7b694843adba2072bf68c96cc88090b6e88
czrchen <130270951+czrchen@users.noreply.github.com>
2024-03-13 17:45:59 +0800

    Add files via upload

---
+++ amazon clone/script.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll('.image-slider ul img');
+const previous = document.querySelector('.previous-button');
+const next = document.querySelector('.next-button');
+
+let n = 0;
+
+function Slide(){
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none';
+    }
+    images[n].style.display = 'block';
+}
+
+Slide();
+
+previous.addEventListener('click',(e)=>{
+    if(n > 0 ){
+        n --;
+    }else{
+        n = images.length - 1;
+    }
+    Slide();
+})
+
+next.addEventListener('click',(e)=>{
+    if(n < images.length - 1 ){
+        n ++ ;
+    }else{
+        n = 0;
+    }
+    Slide();
+})
+
+
